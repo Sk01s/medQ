cc6859ba4ed50a275ffb27e66d60d4dbd32dfe4e
youssef sk <alsarakibiy@gmail.com>
2023-10-23 19:10:48 +0300

    bug

--- pages/_app.js
+++ pages/_app.js
@@ -1,66 +1,67 @@
-import '../public/css/bootstrap.min.css'
-import '../public/css/animate.min.css'
-import '../public/css/boxicons.min.css'
-import '../public/css/flaticon.css'
-import 'react-accessible-accordion/dist/fancy-example.css'
-import 'react-toastify/dist/ReactToastify.css'
-import 'react-18-image-lightbox/style.css';
+import "../public/css/bootstrap.min.css";
+import "../public/css/animate.min.css";
+import "../public/css/boxicons.min.css";
+import "../public/css/flaticon.css";
+import "react-accessible-accordion/dist/fancy-example.css";
+import "react-toastify/dist/ReactToastify.css";
+import "react-18-image-lightbox/style.css";
 import "swiper/css";
 import "swiper/css/bundle";
 
-import '../public/css/style.css'
-import '../public/css/admin.css'
-import '../public/css/responsive.css'
-import '../public/css/loader.css'
-import '../public/css/rtl.css'
+import "../public/css/style.css";
+import "../public/css/admin.css";
+import "../public/css/responsive.css";
+import "../public/css/loader.css";
+import "../public/css/rtl.css";
 
-import { RecoilRoot } from 'recoil'
-import axios from 'axios'
-import { parseCookies, destroyCookie } from 'nookies';
-import Layout from '../components/_App/Layout'
-import { redirectUser } from '../utils/auth';
-import baseUrl from '../utils/baseUrl'
+import { RecoilRoot } from "recoil";
+import axios from "axios";
+import { parseCookies, destroyCookie } from "nookies";
+import Layout from "../components/_App/Layout";
+import { redirectUser } from "../utils/auth";
+import baseUrl from "../utils/baseUrl";
 
 const MyApp = ({ Component, pageProps }) => {
-	return (
-		<RecoilRoot>
-			<Layout {...pageProps}>
-				<Component {...pageProps} />
-			</Layout>
-		</RecoilRoot>
-	)
-}
+    return (
+        <RecoilRoot>
+            <Layout {...pageProps}>
+                <Component {...pageProps} />
+            </Layout>
+        </RecoilRoot>
+    );
+};
 
 MyApp.getInitialProps = async ({ Component, ctx }) => {
-	const { medq_token } = parseCookies(ctx);
-	let pageProps = {}
+    const { medq_token } = parseCookies(ctx);
+    let pageProps = {};
 
-	if(Component.getInitialProps){
-		pageProps = await Component.getInitialProps(ctx)
-	}
+    if (Component.getInitialProps) {
+        pageProps = await Component.getInitialProps(ctx);
+    }
 
-	if(!medq_token){
-		const isProtectedRoute = ctx.pathname === '/admin/dashboard'
-		if(isProtectedRoute){
-			redirectUser(ctx, '/auth')
-		}
-	} else {
-		try {
-			const payload = { headers: { Authorization: medq_token } }
-			const url = `${baseUrl}/api/users/account`
-			const response = await axios.get(url, payload)
-			const user = response.data
-			const isAdmin = user.role == 'admin';
-			const isNotPermitted = !isAdmin && (ctx.pathname === '/admin/dashboard')
-			if(isNotPermitted){
-				redirectUser(ctx, '/products');
-			}
-			pageProps.user = user
-		} catch (error) {
-			destroyCookie(ctx, "medq_token");
-		}
-	}
-	return { pageProps }
-}
+    if (!medq_token) {
+        const isProtectedRoute = ctx.pathname === "/admin/dashboard";
+        if (isProtectedRoute) {
+            redirectUser(ctx, "/auth");
+        }
+    } else {
+        try {
+            const payload = { headers: { Authorization: medq_token } };
+            const url = `${baseUrl}/api/users/account`;
+            const response = await axios.get(url, payload);
+            const user = response.data;
+            const isAdmin = user.role == "admin";
+            const isNotPermitted =
+                !isAdmin && ctx.pathname === "/admin/dashboard";
+            if (isNotPermitted) {
+                redirectUser(ctx, "/products");
+            }
+            pageProps.user = user;
+        } catch (error) {
+            destroyCookie(ctx, "medq_token");
+        }
+    }
+    return { pageProps };
+};
 
-export default MyApp
+export default MyApp;
--- utils/connectDb.js
+++ utils/connectDb.js
@@ -1,23 +1,33 @@
-import mongoose from 'mongoose';
-const connection = {}
+import mongoose from "mongoose";
+const connection = {};
 
 const connectDb = async () => {
-    if(connection.isConnected){
+    if (connection.isConnected) {
         // Using existing database connection
         // console.log("Using existing connection")
         return;
     }
     //Use new database connection
-    const db = await mongoose.connect(process.env.MONGO_SRV, {
-        useCreateIndex: true,
-        useFindAndModify: false,
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    });
+    console.log("DB Before");
+    const db = await mongoose
+        .connect(process.env.MONGO_SRV, {
+            useCreateIndex: true,
+            useFindAndModify: false,
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+        .then((e) => {
+            console.log("connect");
+            return e;
+        })
+        .catch((e) => {
+            console.error("connect");
+            return e;
+        });
 
-    // console.log("DB Connected");
+    console.log("DB Connected");
 
-    connection.isConnected = db.connections[0].isReadyState;
-} 
+    connection.isConnected = db.connections?.[0].isReadyState;
+};
 
 export default connectDb;
